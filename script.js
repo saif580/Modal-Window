49be974c05a63aa63fa7ec8c5cf65989d95e7324
saif580 <saifulhasan580@gmail.com>
2023-05-04 13:43:58 +0530

    modal disappearing feature

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ const closeModal=function(){
 }
 
 const openModal=function(){
-    console.log('Button Clicked!');
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 }
@@ -23,3 +22,9 @@ for(let i=0;i<btnOpenModal.length;i++){
 btnCloseModal.addEventListener('click',closeModal)
 
 overlay.addEventListener('click',closeModal)
+
+document.addEventListener('keydown',function(event){
+    if(event.key==="Escape" && !modal.classList.contains('hidden')){
+        closeModal();
+    }
+})
